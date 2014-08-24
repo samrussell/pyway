3940d802cbdc0d43d4722cbd47906e9cd6f34ed8
Sam Russell <sam.h.russell@gmail.com>
2014-08-24 13:56:20 +1200

    tests for all 4 messages working

--- bgp/messages.py
+++ bgp/messages.py
@@ -2,6 +2,9 @@
 
 import struct
 
+def printhexstring(hexstring):
+  return ''.join(['%02X' % ord(x) for x in hexstring])
+
 class BGPMessage(object):
   factory = {}
 
@@ -65,6 +68,13 @@ class BGPMessageOpen(BGPMessage):
 
 BGPMessage.factory[1] = BGPMessageOpen
 
+def nlritoprefix(nlrituple):
+  # pad to 4 0s, assume IPv4
+  nlriprefix = nlrituple[0] + ''.join([chr(0) for x in range(4 - len(nlrituple[0]))])
+  nlrilen = nlrituple[1]
+  prefix = '.'.join(['%d' % ord(x) for x in nlriprefix])
+  return '%s/%d' % (prefix, nlrilen)
+
 class BGPMessageUpdate(BGPMessage):
   def __init__(self):
     super(BGPMessageUpdate, self).__init__()
@@ -75,6 +85,7 @@ class BGPMessageUpdate(BGPMessage):
     self.totalpathattributeslength=0
     self.pathattributes=struct.pack('')
     self.nlri=struct.pack('')
+    self.nlrilist = []
 
   def encode(self):
     header = super(BGPMessageUpdate, self).encode()
@@ -97,26 +108,63 @@ class BGPMessageUpdate(BGPMessage):
     nlrioffset = message.totalpathattributeslength+2+pathattributeoffset
     message.pathattributes = payload[pathattributeoffset+2:nlrioffset]
     message.nlri=payload[nlrioffset:]
+    # decode nlri
+    i = 0
+    while i < len(message.nlri):
+      (nlrilen, ) = struct.unpack('!B', message.nlri[i:i+1])
+      nlribytelen = nlrilen/8 + (1 if (nlrilen%8 > 0) else 0)
+      i = i+1
+      if nlribytelen+i > len(message.nlri):
+        raise Exception('Bad NLRI - length is too damn high %d %d %d', nlribytelen, i, len(message.nlri))
+      nlriprefix = message.nlri[i:i+nlribytelen]
+      message.nlrilist.append((nlriprefix, nlrilen))
+      i = i + nlribytelen
     return message
 
   def __str__(self):
-    return "BGP Update message, length %d, withdrawnrouteslength %d, pathattributeslength %d" % (self.length, self.withdrawnrouteslength, self.totalpathattributeslength)
+    return "BGP Update message, length %d, withdrawnrouteslength %d, pathattributeslength %d nlris: %s" % (self.length, self.withdrawnrouteslength, self.totalpathattributeslength, ','.join([nlritoprefix(x) for x in self.nlrilist]))
 
 BGPMessage.factory[2] = BGPMessageUpdate
 
-class BGPMessageKeepalive(BGPMessage):
-  def __init__(self):
-    super(BGPMessageKeepalive, self).__init__()
-    self.length=19
-    self.messagetype=3
-
-  def encode(self):
-    header = super(BGPMessageKeepalive, self).encode()
-    return header
-
-BGPMessage.factory[3] = BGPMessageKeepalive
-
 class BGPMessageNotification(BGPMessage):
+
+  errordecode = {
+                      1 : 'Message Header Error',
+                      2 : 'OPEN Message Error',
+                      3 : 'UPDATE Message Error',
+                      4 : 'Hold Timer Expired',
+                      5 : 'Finite State Machine Error',
+                      6 : 'Cease',
+      }
+  suberrordecode = { 
+                      1 : {
+                        1 : 'Connection Not Synchronized.',
+                        2 : 'Bad Message Length.',
+                        3 : 'Bad Message Type.',
+                        },
+                      2 : {
+                        1 : 'Unsupported Version Number.',
+                        2 : 'Bad Peer AS.',
+                        3 : 'Bad BGP Identifier.',
+                        4 : 'Unsupported Optional Parameter.',
+                        5 : '[Deprecated - see Appendix A]. (RFC 4271)',
+                        6 : 'Unacceptable Hold Time.',
+                        },
+                      3 : {
+                        1 : 'Malformed Attribute List.',
+                        2 : 'Unrecognized Well-known Attribute.',
+                        3 : 'Missing Well-known Attribute.',
+                        4 : 'Attribute Flags Error.',
+                        5 : 'Attribute Length Error.',
+                        6 : 'Invalid ORIGIN Attribute.',
+                        7 : '[Deprecated - see Appendix A]. (RFC 4271)',
+                        8 : 'Invalid NEXT_HOP Attribute.',
+                        9 : 'Optional Attribute Error.',
+                        10 : 'Invalid Network Field.',
+                        11 : 'Malformed AS_PATH.',
+                        },
+    }
+
   def __init__(self):
     super(BGPMessageNotification, self).__init__()
     self.length=21
@@ -129,5 +177,49 @@ class BGPMessageNotification(BGPMessage):
     header = super(BGPMessageNotification, self).encode()
     return header + struct.pack('!BB', self.errorcode, self.errorsubcode) + self.data
 
-BGPMessage.factory[4] = BGPMessageNotification
+  @staticmethod
+  def decode(payload, length):
+    message = BGPMessageNotification()
+    message.length = length+19
+    if message.length<21:
+      raise Exception('BGP Notification message less than 21 bytes, only %d bytes' % message.length)
+    (message.errorcode, message.errorsubcode) = struct.unpack('!BB', payload[:2])
+    message.data = payload[2:]
+    return message
+
+  def __str__(self):
+    errortext = 'code not found'
+    suberrortext = 'code not found'
+    try:
+      errortext = BGPMessageNotification.errordecode[self.errorcode]
+      suberrortext = BGPMessageNotification.suberrordecode[self.errorcode][self.errorsubcode]
+    except:
+      pass
+    return 'BGP Notification message length %d error %d [%s] suberror %d [%s] data %s' % (self.length, self.errorcode, errortext, self.errorsubcode, suberrortext, printhexstring(self.data))
+
+BGPMessage.factory[3] = BGPMessageNotification
+
+class BGPMessageKeepalive(BGPMessage):
+  def __init__(self):
+    super(BGPMessageKeepalive, self).__init__()
+    self.length=19
+    self.messagetype=3
+
+  def encode(self):
+    header = super(BGPMessageKeepalive, self).encode()
+    return header
+
+  @staticmethod
+  def decode(payload, length):
+    message = BGPMessageKeepalive()
+    message.length = length+19
+    # basic sanity check
+    if message.length!=19:
+      raise Exception('Keepalive message with trash on the end, length: %d' % message.length)
+    return message
+
+  def __str__(self):
+    return 'BGP Keepalive message length %d' % self.length
+
+BGPMessage.factory[4] = BGPMessageKeepalive
 
